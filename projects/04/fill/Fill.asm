fff931e681fbd33e73725a6b6824176f3f10906c
Xinyu <victorvone@gmail.com>
2019-12-10 22:21:31 +0800

    Add Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+// addr = SCREEN
+@SCREEN
+D=A
+@addr
+M=D
+
+// i = 0
+@0
+D=A
+@i
+M=D
+
+// n = 8192
+@8192
+D=A
+@n
+M=D
+
+// check k value
+@KBD
+D=M
+
+// if k isn't 0, turn black
+@BLACK
+D;JNE
+
+// if k is 0, turn white
+@WHITE
+0;JMP
+
+(BLACK)
+// if i >= n, jump to LOOP
+@i
+D=M
+@n
+D=D-M
+@LOOP
+D;JGE
+
+// RAM[addr+i] = -1
+@i
+D=M
+@addr
+A=D+M
+M=-1
+
+// i+=1
+@i
+M=M+1
+
+// continue to next 16bit word
+@BLACK
+0;JMP
+
+
+(WHITE)
+// if i >= n, jump to LOOP
+@i
+D=M
+@n
+D=D-M
+@LOOP
+D;JGE
+
+// RAM[addr+i] = 0
+@i
+D=M
+@addr
+A=D+M
+M=0
+
+// i+=1
+@i
+M=M+1
+
+// continue to next 16bit word
+@WHITE
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// sum = 0
+@sum
+M=0
+
+(LOOP)
+// if R1 = 0, go to stop
+@R1
+D=M
+@STOP
+D;JEQ
+
+// sum += RAM[R0]
+@R0
+D=M
+@sum
+M=D+M
+
+// RAM[R1] -= 1
+@R1
+M=M-1
+
+// go to LOOP
+@LOOP
+0;JMP
+
+(STOP)
+// RAM[R1] = sum
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
